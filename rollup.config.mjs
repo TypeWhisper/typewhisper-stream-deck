a583cd4d99b128adb7e32cecaeaed589a1730c4b
Marco Hillger <marco@seofood.de>
2026-07-22 00:33:59 +0200

    Package Windows clipboard helper

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import fs from "node:fs";
 import path from "node:path";
 import url from "node:url";
 
@@ -37,6 +38,17 @@ const config = {
 		}),
 		commonjs(),
 		!isWatching && terser(),
+		{
+			name: "copy-clipboardy-fallbacks",
+			writeBundle() {
+				const sourceDirectory = path.resolve("node_modules/clipboardy/fallbacks/windows");
+				const targetDirectory = path.resolve(sdPlugin, "fallbacks/windows");
+				fs.mkdirSync(targetDirectory, { recursive: true });
+				for (const fileName of ["clipboard_i686.exe", "clipboard_x86_64.exe"]) {
+					fs.copyFileSync(path.join(sourceDirectory, fileName), path.join(targetDirectory, fileName));
+				}
+			}
+		},
 		{
 			name: "emit-module-package-file",
 			generateBundle() {
